17d4b09a3a877196c84da50dbb7c951975d05b7b
SumeyyeYuce <yucesumeyye25@gmail.com>
2022-12-02 18:00:07 +0300

    2 Aralık

---
+++ week_09/2-12-2022/BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,25 @@
+﻿using BlogApp.Services.Abstract;
+using BlogApp.Shared.Utilities.Result.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public async  Task <IActionResult> Index()
+        {
+            var result = await _articleService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)//eger result status succes olmuşsa dataya view'e yolluyoruz.
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
