436ef46da7d80397f0e46a338341f8296ad5dc4c
SumeyyeYuce <yucesumeyye25@gmail.com>
2022-12-09 18:12:17 +0300

    9 Aralık

---
+++ week_10/09-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Business.Abstract;
+using ShoppingApp.Core;
+using ShoppingApp.Entity.Concrete;
+using ShoppingApp.Web.Areas.Admin.Models.Dtos;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoryListDto = new CategoryListDto
+            {
+                Categories = categories
+            };
+            return View(categoryListDto);
+        }
+        [HttpGet]
+        public IActionResult Create()//bu metodun varlık sebebi kategorileri girilmesi gereken formu açıcak
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryAddDto categoryAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                //modelState is valid aşşagdıaki  işlemleri yapıp kayıt yapıcak valide değilse kategori indexsin gitsin
+                //valid oldugunda da gitsin
+                var category = new Category
+                {
+                    Name = categoryAddDto.Name,
+                    Description = categoryAddDto.Description,
+                    Url=Jobs.InitUrl(categoryAddDto.Name)
+
+                };
+                await _categoryService.CreateAsync(category);
+
+                return RedirectToAction("Index");//yukarıdaki işlemlmer bitince tekrar index' e gelsin
+                //return RedirectToAction(nameof(Index));
+            }
+            return View();//bu creat.cshtml'i çagırır
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);//ilgili kategoriyi bulup getiricek
+            if (category==null)//kategori bilgiis boşsa hata mesajı ver
+            {
+                return NotFound();
+            }
+            var categoryUpdateDto = new CategoryUpdateDto
+            {
+                Id=category.Id,
+                Name = category.Name,
+                Description = category.Description,
+                Url = category.Url
+            };
+            return View(categoryUpdateDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryUpdateDto categoryUpdateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = await _categoryService.GetByIdAsync(categoryUpdateDto.Id);//dışarıdan gelen 
+                if (category==null)
+                {
+                    return NotFound();
+                }
+                category.Name = categoryUpdateDto.Name;
+                category.Description = categoryUpdateDto.Description;
+                category.Url = Jobs.InitUrl(categoryUpdateDto.Name);
+                
+                _categoryService.Update(category);//yukarıda oluşturudgumuz category yi update etmesi için yolladık
+                return RedirectToAction("Index");
+            }
+            return View(categoryUpdateDto);//eger isvalid boş dönerse categoryUpdatDto daki hata mesajlarını göstererk döndür diyoruz
+        }
+
+        [HttpGet]//herhanbgi bir formun içinde olmaıgımız için post yapmadık get yaptık
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _categoryService.Delete(category);
+            return RedirectToAction("Index");
+        }
+
+      
+      
+    }
+}
---
+++ week_10/09-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/ProductAddDto.cs
@@ -0,0 +1,46 @@
+﻿using ShoppingApp.Entity.Concrete;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class ProductAddDto
+    {
+
+        [DisplayName("Ürün Adı")] //ekranda gözükücek isim
+        [Required(ErrorMessage = "{0}  boş bırakılmamalıdır")]
+        [MinLength(5, ErrorMessage = "{0} , {1} karakter kısa olmamaldır")]
+        [MaxLength(100, ErrorMessage = "{0} , {1} karakterden uzun olmamaıldır")]
+
+        public string Name { get; set; }
+
+
+        [DisplayName("Ürün Fiyatı")] 
+        [Required(ErrorMessage = "{0}  boş bırakılmamalıdır")]
+        public decimal? Price { get; set; }
+
+        [DisplayName("Açıklama")] 
+        [Required(ErrorMessage = "{0}  boş bırakılmamalıdır")]
+        [MinLength(5, ErrorMessage = "{0} , {1} karakter kısa olmamaldır")]
+        [MaxLength(500, ErrorMessage = "{0} , {1} karakterden uzun olmamaıldır")]
+        public string Description { get; set; }
+
+        [DisplayName("Ürün Resmi")] 
+        [Required(ErrorMessage = "{0}  seçilmelidir")]
+        public IFormFile ImageFile { get; set; }
+
+        [DisplayName("Anasayfa ürünü")] 
+        public bool IsHome { get; set; }
+
+
+        [DisplayName("Onaylı ürün?")] 
+        public bool IsApproved { get; set; }
+
+        [DisplayName("Kategoriler")] 
+        public List<Category>  Categories { get; set; }
+
+        [Required(ErrorMessage = "En az bir kategori seçilmeli")]
+        public List<Category> SelectedCategories { get; set; }
+
+    }
+}
