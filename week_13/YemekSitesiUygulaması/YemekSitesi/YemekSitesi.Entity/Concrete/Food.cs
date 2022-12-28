542735030046d3fc39aedb229190876314911cd2
SumeyyeYuce <yucesumeyye25@gmail.com>
2022-12-28 21:59:38 +0300

    YemekSitesi

--- week_13/YemekSitesiUygulaması/YemekSitesi/YemekSitesi.Entity/Concrete/Food.cs
+++ week_13/YemekSitesiUygulaması/YemekSitesi/YemekSitesi.Entity/Concrete/Food.cs
@@ -14,10 +14,10 @@ namespace YemekSitesi.Entity.Concrete
         public string Description { get; set; }//Açıklama
         public string ImageUrl { get; set; }//Resim
         public string Url { get; set; }//url uzantı
-        public string IsHome { get; set; }//anasayfa ürünü
-        public string IsApproved { get; set; }//Onaylı mı
+        public bool IsHome { get; set; }//anasayfa ürünü
+        public bool IsApproved { get; set; }//Onaylı mı
         public DateTime DateAdded { get; set; }//oluşturulma tarihi
-        public int CookingTime { get; set; }//pişirme süresi
+        public string CookingTime { get; set; }//pişirme süresi
         public List<FoodCategory> FoodCategories { get; set; }//hangi categoryde
 
 
