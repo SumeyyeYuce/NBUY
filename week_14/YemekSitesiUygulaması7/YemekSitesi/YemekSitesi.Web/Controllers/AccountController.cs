3521151834721e0f90c0ed7eb3430818a5540b37
SumeyyeYuce <yucesumeyye25@gmail.com>
2023-01-06 11:22:10 +0300

    yemekSitesi

---
+++ week_14/YemekSitesiUygulaması7/YemekSitesi/YemekSitesi.Web/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YemekSitesi.Entity.Concrete.Identity;
+
+using YemekSitesi.Web.Models.Dtos;
+
+namespace YemekSitesi.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+      
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginDto { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(loginDto.UserName);
+                if (user==null)
+                {
+                    ModelState.AddModelError("", "Böyle bir kullanıcı bulunamadı!");
+                    return View(loginDto);
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password,true,true);
+                if (result.Succeeded)
+                {
+                    return Redirect(loginDto.ReturnUrl ?? "~/");
+                }
+                ModelState.AddModelError("", "Email adresi ya da parola hatalı!");
+            }
+            return View(loginDto);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    FirstName = registerDto.FirstName,
+                    LastName = registerDto.LastName,
+                    Email = registerDto.Email,
+                    UserName = registerDto.UserName  
+                };
+                var result  = await _userManager.CreateAsync(user,registerDto.Password);
+                if (result.Succeeded)
+                {
+                    var tokenCode = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var url = Url.Action("ConfirmEmail", "Account", new
+                    {
+                        userId = user.Id,
+                        token = tokenCode
+                    });
+
+                    //await _emailSender.SendEmailAsync(user.Email, "ShoppingApp Hesap Onaylama", $"<h1>Merhaba</h1>" +
+                    //   $"<br>" +
+                    //   $"<p>Lütfen hesabınızı onaylamak için aşağıdaki linke tıklayınız.</p>" +
+                    //   $"<a href='https://localhost:5178{url}'></a>");
+                    //ViewBag.Message = "Kayıt işleminizi tamamlamak için mailinize gönderilen onaylama linkine tıklayınız.";
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+            return View(registerDto);
+        }
+    }
+}
--- week_14/YemekSitesiUygulaması7/YemekSitesi/YemekSitesi.Web/Program.cs
+++ week_14/YemekSitesiUygulaması7/YemekSitesi/YemekSitesi.Web/Program.cs
@@ -1,18 +1,68 @@
-﻿using YemekSitesi.Business.Abstract;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using YemekSitesi.Business.Abstract;
 using YemekSitesi.Business.Concrete;
 using YemekSitesi.Data.Abstract;
 using YemekSitesi.Data.Concrete;
 using YemekSitesi.Data.Concrete.EfCore.Contexts;
+using YemekSitesi.Entity.Concrete.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlite("Data Source=YemekSitesi.db"));
 builder.Services.AddDbContext<YemekSitesiContext>();
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6; 
+    options.Password.RequireNonAlphanumeric = true;
+
+    options.Lockout.MaxFailedAccessAttempts = 5; 
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(15);
+
+    options.User.RequireUniqueEmail = true;
+
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/account/login";
+    options.LogoutPath = "/account/logout";
+    options.AccessDeniedPath = "/account/accessdenied";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.Cookie = new CookieBuilder
+    {
+        HttpOnly = true,
+        Name = ".YemekSitesi.Security.Cookie",
+        SameSite = SameSiteMode.Strict
+    };
+});
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<IFoodService, FoodManager>();    
+builder.Services.AddScoped<IFoodService, FoodManager>();
+
+//builder.Services.AddScoped<IEmailSender, SmtpEmailSender>(x => new SmtpEmailSender(
+//"smtp.office365.com",587, true, "wissen_deneme_d@hotmail.com","12345wissen"
+  //  ));
+
+
+
 
 var app = builder.Build();
 
@@ -27,6 +77,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
