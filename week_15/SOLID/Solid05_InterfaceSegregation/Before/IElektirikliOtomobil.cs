0eb4108cbed442af1a8d14a3d5eb932d94297280
SumeyyeYuce <yucesumeyye25@gmail.com>
2023-01-10 15:04:45 +0300

    10 ocak

---
+++ week_15/SOLID/Solid05_InterfaceSegregation/Before/IElektirikliOtomobil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid05_InterfaceSegregation.Before
+{
+    public interface IElektirikliOtomobil
+    {
+        public string BataryaTipi { get; set; }
+        public string BataryaTipiniGetir();
+        public decimal VoltajDegeriniGetir();
+
+    }
+}
---
+++ week_15/SOLID/Solid06_DependencyInversion/Program.cs
@@ -0,0 +1,24 @@
+﻿//using Solid06_DependencyInversion.Before;
+using Solid06_DependencyInversion.After;
+
+
+namespace Solid06_DependencyInversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region before
+            //MailService mailService =  new MailService();
+            //mailService.SendMail(new GmailServer());
+            #endregion
+
+            #region After
+            MailService mailService = new MailService();
+            mailService.SendMail(new GmailServer(),"sum.gmail.com","selam");
+            mailService.SendMail(new HotmailServer(), "sum.gmail.com", "selammmmmm");
+            #endregion
+
+        }
+    }
+}
