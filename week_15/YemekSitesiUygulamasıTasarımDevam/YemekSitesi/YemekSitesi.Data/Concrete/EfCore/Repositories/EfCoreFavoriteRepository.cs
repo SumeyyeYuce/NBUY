2ee09ba218aba510424d8cf9b572252389e13e5f
SumeyyeYuce <yucesumeyye25@gmail.com>
2023-01-14 18:47:43 +0300

    YemekSitesi

---
+++ week_15/YemekSitesiUygulamasıTasarımDevam/YemekSitesi/YemekSitesi.Data/Concrete/EfCore/Repositories/EfCoreFavoriteRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YemekSitesi.Data.Abstract;
+using YemekSitesi.Data.Concrete.EfCore.Contexts;
+using YemekSitesi.Entity.Concrete;
+
+namespace YemekSitesi.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreFavoriteRepository : EfCoreGenericRepository<Favorite>, IFavoriteRepository
+    {
+        public EfCoreFavoriteRepository(YemekSitesiContext context) : base(context)
+        {
+
+        }
+
+        private YemekSitesiContext YemekSitesiContext
+        {
+            get { return _context as YemekSitesiContext; }
+        }
+
+        public async Task AddToFavorite(string userId, int foodId, int favortiteNumber)
+        {
+            var favorite = await GetFavByUserId(userId);
+            if (favorite != null)
+            {
+                var index = favorite.FavoriteItems.FindIndex(fi => fi.FoodId == foodId);
+
+                if (index<0)
+                {
+                    favorite.FavoriteItems.Add(new FavoriteItem
+                    {
+                        FoodId = foodId,    
+                        FavoriteId= favorite.Id,
+                        FavortiteNumber = favortiteNumber   
+                    });
+                }
+                else
+                {
+                    favorite.FavoriteItems[index].FavortiteNumber += favortiteNumber;
+                }
+                YemekSitesiContext.Favorites.Update(favorite);
+            }
+        }
+
+        public async Task<Favorite> GetFavByUserId(string userId)
+        {
+            var favorite = await YemekSitesiContext
+                .Favorites
+                .Include(f => f.FavoriteItems)
+                .ThenInclude(fi => fi.Food)
+                .FirstOrDefaultAsync(f => f.UserId == userId);
+            return favorite;
+        }
+    }
+}
---
+++ week_15/YemekSitesiUygulamasıTasarımDevam/YemekSitesi/YemekSitesi.Data/Config/CategoryConfig.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YemekSitesi.Entity.Concrete;
+
+namespace YemekSitesi.Data.Config
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c=>c.Id).ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(c => c.Url)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.ToTable("Categories");//Veritabanındaki tablonun adı
+
+            builder.HasData(
+                
+                new Category
+                {
+                        Id=1,
+                        Name="Çorba",
+                        Description="Sizler için özenle hazırlanmış Çorba Tariflerimiz burada bulunmaktadır",
+                        Url="corba"
+                },              
+                new Category
+                {
+                         Id = 2,
+                         Name = "Kurabiye",
+                         Description = "Sizler için özenle hazırlanmış Kurabiye Tariflerimiz burada bulunmaktadır",
+                         Url = "kurabiye"
+                },
+                new Category
+                {
+                          Id = 3,
+                          Name = "Tatlı",
+                          Description = "Sizler için özenle hazırlanmış Tatlı Tariflerimiz burada bulunmaktadır",
+                          Url = "tatlı"
+                }
+                //new Category
+                //{
+                //           Id = 5,
+                //           Name = "Sebze",
+                //           Description = "Sizler için özenle hazırlanmış Sebze Tariflerimiz burada bulunmaktadır",
+                //           Url = "sebze"
+                //},
+                //new Category
+                //{
+                //            Id = 6,
+                //            Name = "Hamur İşi",
+                //            Description = "Sizler için özenle hazırlanmış Hamur İşi Tariflerimiz burada bulunmaktadır",
+                //            Url = "hamur-isi"
+                //}, 
+                //new Category
+                //{
+                //            Id = 7,
+                //            Name = "Salata",
+                //            Description = "Sizler için özenle hazırlanmış Salata Tariflerimiz burada bulunmaktadır",
+                //            Url = "salata"
+                //}, 
+                //new Category
+                //{
+                //            Id = 8,
+                //            Name = "Aperatif",
+                //            Description = "Sizler için özenle hazırlanmış Aperatif Tariflerimiz burada bulunmaktadır",
+                //            Url = "aperatif"
+                //}, 
+                //new Category
+                //{
+                //            Id = 9,
+                //            Name = "Et Yemekleri",
+                //            Description = "Sizler için özenle hazırlanmış Et Yemekleri Tariflerimiz burada bulunmaktadır",
+                //            Url = "et-yemekleri"
+                //},
+                //new Category
+                //{
+                //             Id = 10,
+                //             Name = "Kahvaltılık",
+                //             Description = "Sizler için özenle hazırlanmış Kahvaltılık Tariflerimiz burada bulunmaktadır",
+                //             Url = "kahvaltılık"
+                //}
+
+
+                );
+            
+        }
+    }
+}
